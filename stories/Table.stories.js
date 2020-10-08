9a7caf4e17a9a0fbc254ae335a02b8a89fcb913a
Aravindasamy, Rajaprabhu (ESI) <rajaprabhu.aravindasamy@adp.com>
2020-10-08 17:48:26 +0530

    * Added few stories to prepare a demo

--- stories/Table.stories.js
+++ stories/Table.stories.js
@@ -4,7 +4,7 @@ import { Table } from '../src';
 
 export default { title: 'Table' }
 
-export const SimpleTable = () =>
+export const Simple_Table = () =>
   <Table
     columnDefinition={[
       { title: 'Name', 'dataIdentifier': 'name', key: 'name' },
@@ -14,7 +14,7 @@ export const SimpleTable = () =>
       { name: 'Jon', age: '35', key: 'jn' }]}
   />;
 
-export const TableWithLargeData = () =>
+export const Table_With_Large_Data = () =>
   <Table
     columnDefinition={[
       { title: 'Name', 'dataIdentifier': 'name', key: 'name' },
@@ -43,7 +43,53 @@ export const TableWithLargeData = () =>
       { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zxs123' },]}
   />;
 
-export const TableWithFixedHeader = () =>
+export const Table_With_Fixed_Header = () =>
+  <Table
+    isHeaderFixed={true}
+    columnDefinition={[
+      { title: 'Name', 'dataIdentifier': 'name', key: 'name' },
+      { title: 'Age', 'dataIdentifier': 'age', key: 'age' },
+      { title: 'Gender', 'dataIdentifier': 'gender', key: 'gender' },
+      { title: 'Address', 'dataIdentifier': 'address', key: 'address' },
+      { title: 'District', 'dataIdentifier': 'district', key: 'district' },
+      { title: 'State', 'dataIdentifier': 'state', key: 'state' },
+      { title: 'Country', 'dataIdentifier': 'country', key: 'country' }]}
+    datasource={[
+      { name: 'Rajaprabhu - First', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'rj' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'pj' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'cj' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zx' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'rjs' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'pjs' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'caj' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zxs' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'r2j' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'p12j' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'c12j' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zx42' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'r244js' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'pj231s' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'ca312j' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zxs123' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'rj221' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'pj312' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'cjadad' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zxdassd' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'rjdasds' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'pjrfvs' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'cajdbbf' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zxsfggd' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'r2jdasw' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'p12jdasd' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'c12jassd' },
+      { name: 'Kavya P', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zx42ww3' },
+      { name: 'Rajaprabhu', age: '29', gender: 'Male', address: 'Brook street, 3rd sector', district: 'Newyork', state: 'Tamil nadu', country: 'United states', key: 'r244jssdad' },
+      { name: 'Jonathan', age: '32', gender: 'Male', address: 'Bus street, 13rd sector', district: 'California', state: 'Andhra pradesh', country: 'India', key: 'pj231dasds' },
+      { name: 'Sam shields', age: '34', gender: 'Male', address: 'New street, 23rd sector', district: 'Kanchipuram', state: 'Kerala', country: 'Russia', key: 'ca312jasdasd' },
+      { name: 'Kavya P - Last', age: '39', gender: 'Female', address: 'Old street, 323rd sector', district: 'Texas', state: 'Maharashtra', country: 'China', key: 'zxs123weqe' },]}
+  />;
+
+  export const Table_With_Fixed_Header_And_Static_Lazy_Load = () =>
   <Table
     staticDataLazyLoadDefinition={{ rowsPerLoad: 10 }}
     isHeaderFixed={true}
